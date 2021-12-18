50f0235f350e3ec31e1e9e187eb9d3f385d45318
Christian Fuchs <kontaktierbar@gmail.com>
2021-12-18 03:01:58 +0100

    add some more c++ pseudo code

--- C++/backing-code/flight-dto.h
+++ C++/backing-code/flight-dto.h
@@ -1,1 +1,8 @@
-class FlightDto {};
+#include <string>
+
+struct FlightDto {
+    const std::string originCity;
+    const std::string originAirportCode;
+    const std::string destinationCity;
+    const std::string destinationAirportCode;
+};
