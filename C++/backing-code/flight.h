220840ca0165c966f15f937d8e8a1ac38c9db6c3
Christian Fuchs <kontaktierbar@gmail.com>
2021-12-19 00:01:24 +0100

    add Flight constructor

--- C++/backing-code/flight.h
+++ C++/backing-code/flight.h
@@ -1,2 +1,10 @@
+#include <string>
+
 class Flight {
+private:
+    const std::string flightNumber;
+public:
+    Flight(std::string &flightNumber)
+    : flightNumber(flightNumber)
+    {}
 };
--- C++/smelly-tests/eager_test.cpp
+++ C++/smelly-tests/eager_test.cpp
@@ -3,8 +3,13 @@
 #include <flight.h>
 
 class EagerTest : public ::testing::Test {
+public:
+    std::string validFlightNumber = "1";
 };
 
 // Demonstrate some basic assertions.
 TEST_F(EagerTest, TestFlightMileage_asKm2) {
+    // set up fixture
+    // exercise constructor
+    Flight newFlight(validFlightNumber);
 }
